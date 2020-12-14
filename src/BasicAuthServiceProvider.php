c366aafe3641c66008016c4e70e08f6402db2e98
Lucas Ferreira <lucas.ferreira@convergenceworks.com.br>
2020-12-14 19:15:41 -0300

    basic auth middleware

---
+++ src/BasicAuthServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace KasFerreira\BasicAuth;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class BasicAuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+       
+        
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+
+
+    }
+}
---
+++ src/Http/Middleware/BasicAuth.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace KasFerreira\BasicAuth\Http\Middleware;
+
+use Closure;
+
+class BasicAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+       
+        header('Cache-Control: no-cache, must-revalidate, max-age=0');
+        $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+        $is_not_authenticated = (
+            !$has_supplied_credentials ||
+            $_SERVER['PHP_AUTH_USER'] != config('app.basic_auth_user')||
+            $_SERVER['PHP_AUTH_PW']   != config('app.basic_auth_password')
+        );
+        if ($is_not_authenticated) {
+            header('HTTP/1.1 401 Authorization Required');
+            header('WWW-Authenticate: Basic realm="Access denied"');
+            exit;
+        }
+        return $next($request);
+    }
+}
